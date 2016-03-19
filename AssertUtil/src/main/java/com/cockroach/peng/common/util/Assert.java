b09683be80bfb44d39115b70eac5b92cabca8e3d
gangqiang.pgq <pgq827258406@163.com>
2016-03-19 15:11:22 +0800

    add assert and date util

---
+++ AssertUtil/src/main/java/com/cockroach/peng/common/util/Assert.java
@@ -0,0 +1,15 @@
+/**
+ * cockroachPeng.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved.
+ */
+package com.cockroach.peng.common.util;
+
+/**
+ * Assert is used to validate arguments
+ * 
+ * @author gangqiang.pgq
+ * @version $Id: Assert.java, v 0.1 2016年3月19日 下午3:07:03 gangqiang.pgq Exp $
+ */
+public abstract class Assert extends org.springframework.util.Assert {
+
+}
---
+++ DateUtil/src/main/java/com/cockroach/peng/common/util/DateUtil.java
@@ -0,0 +1,15 @@
+/**
+ * cockroachPeng.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved.
+ */
+package com.cockroach.peng.common.util;
+
+/**
+ * DateUtil
+ * 
+ * @author gangqiang.pgq
+ * @version $Id: DateUtil.java, v 0.1 2016年3月19日 下午3:05:00 gangqiang.pgq Exp $
+ */
+public class DateUtil {
+
+}
