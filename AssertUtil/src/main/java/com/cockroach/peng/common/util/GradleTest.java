fa0fb60908c56c26ae52ee0f35d0d6c726119ef9
gangqiang.pgq <pgq827258406@163.com>
2016-03-19 14:46:15 +0800

    update gradle config

---
+++ AssertUtil/src/main/java/com/cockroach/peng/common/util/GradleTest.java
@@ -0,0 +1,17 @@
+/**
+ * cockroach.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved.
+ */
+package com.cockroach.peng.common.util;
+
+/**
+ * gradel test 
+ * 
+ * @author gangqiang.pgq
+ * @version $Id: GradleTest.java, v 0.1 2016年3月19日 上午12:41:18 gangqiang.pgq Exp $
+ */
+public class GradleTest {
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
